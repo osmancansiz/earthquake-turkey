ee6d6ad1bcd1be42fd30072132421897771cbc57
Burkay SEVİLMİŞ <burkay.sevilmis@gmail.com>
2020-01-23 12:30:03 +0300

    Maps Marker Addr

--- App.js
+++ App.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import axios from 'axios';
 import MapView,{Marker} from 'react-native-maps';
 
 export default class App extends Component {
   
     
-    state = {
-        region: {
-            latitude:40.9695026,
-            longitude: 29.1015871,
-            latitudeDelta: 5,
-            longitudeDelta: 5,
+   state = {
+    markers:[]
+  }
+
+  componentDidMount(){
+
+    axios.get('https://api.orhanaydogdu.com.tr/deprem/live.php', {
+  }).then(obj => {
+    for (let index = 0; index < obj.data.result.length; index++) {
+      this.setState({
+        markers:[...this.state.markers,{
+          title: obj.data.result[index].title +' '+obj.data.result[index].mag ,
+          coordinates: {
+            latitude:obj.data.result[index].lat ,
+            longitude: obj.data.result[index].lng
           },
-    };
+        }]
+      })
+      
+    }
+    console.log(this.state.markers)
+      
+  })
 
-
+  }
   render() {
     return (
       <View style={{flex:1}}>
-       <MapView style={{width:'100%',height:'100%'}}
-    region={this.state.region}
-  
+<MapView style={{width:'100%',height:'100%'}}
+  initialRegion={{
+    latitude: 38.9977645,
+    longitude: 34.7361951,
+    latitudeDelta: 5,
+    longitudeDelta: 15,
+  }}
+>
+  {this.state.markers.map(marker => (
+    <MapView.Marker 
+      coordinate={marker.coordinates}
+      title={marker.title}
     
-  >
-
-<Marker
-      coordinate={this.state.region}
     />
-  </MapView>
-
+  ))}
+</MapView>
       </View>
     );
   }
